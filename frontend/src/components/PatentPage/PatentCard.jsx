0b9ed9f178997eb7e698590d4f2b58dee4af6c36
Jordy Araujo <jordy.araujo@hotmail.com>
2021-10-21 01:21:49 -0400

    Add pagination to 'Patents' page

--- frontend/src/components/PatentPage/PatentCard.jsx
+++ frontend/src/components/PatentPage/PatentCard.jsx
@@ -1,17 +1,18 @@
-import React from "react";
-import { Card } from "react-bootstrap";
+import React, { useState } from "react";
+import { Card, Button } from "react-bootstrap";
 import Iframe from 'react-iframe';
 
 // This is a card component that is from bootstrap used to display the patent via an IFrame
-
 const PatentCard = (props) => {
+  const [currentPage, setCurrentPage] = useState(1);
 
   return (
     <Card style={{ width: "100%" }}>
 
       <Card.Body>
         <Iframe
-            url={checkID(props.patents)}
+            id="patent-pdf"
+            url={checkID(props.patents, currentPage)}
             width="100%"
             height="1000px"
             className="size"
@@ -19,6 +20,26 @@ const PatentCard = (props) => {
             postion="relative"
         />
       </Card.Body>
+      <Button 
+        disabled={
+          currentPage <= 1
+        } 
+        onClick={
+          () => { 
+            setCurrentPage(currentPage - 1);
+            document.getElementById("patent-pdf").src = checkID(props.patents, currentPage);
+          }
+        }
+        style={{marginBottom: "1%"}}
+      >Previous Page</Button>
+      <Button 
+        onClick={
+          () => { 
+            setCurrentPage(currentPage + 1);
+            document.getElementById("patent-pdf").src = checkID(props.patents, currentPage); 
+          }
+        }
+      >Next Page</Button>
     </Card>
   );
 }
@@ -26,7 +47,7 @@ const PatentCard = (props) => {
 // This function is used to check the patent documentID and append it to the correct url needed to dispaly.
 // The if statement was created to avoid a sync issue with the state from the parent component
 
-function checkID(patents){
+function checkID(patents, currentPage){
 
   if(patents !== undefined)
   {
@@ -43,20 +64,17 @@ function checkID(patents){
         id = "0" + id;
       }
         
-      return ("https://pdfpiw.uspto.gov/"+id.substring(6, 8)+"/"+id.substring(3, 6)+"/"+id.substring(0, 3)+"/5.pdf")
+      return ("https://pdfpiw.uspto.gov/"+id.substring(6, 8)+"/"+id.substring(3, 6)+"/"+id.substring(0, 3)+"/"+currentPage+".pdf")
 
     }
     else if (patents[0].patentCorpus === "PGPUB") // ex: 20170205789
     {
-      return ("https://pdfaiw.uspto.gov/"+id.substring(9,11)+"/"+id.substring(0,4)+"/"+id.substring(7,9)+"/"+id.substring(4,7)+"/5.pdf")
+      return ("https://pdfaiw.uspto.gov/"+id.substring(9,11)+"/"+id.substring(0,4)+"/"+id.substring(7,9)+"/"+id.substring(4,7)+"/"+currentPage+".pdf")
         
       // DO NOT REMOVE: this website allows us to search for patents by number and extract the API by inspecting the frame URL.
       //return ("https://appft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&u=%2Fnetahtml%2FPTO%2Fsearch-adv.html&r=1&f=G&l=50&d=PG01&p=1&S1="+id+".PGNR.&OS=DN/"+id+"&RS=DN/"+id);
     }
   } 
 }
-
-
-
 
 export default PatentCard;
