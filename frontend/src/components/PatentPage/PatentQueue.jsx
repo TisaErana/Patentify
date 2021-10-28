36196319e8616a093ec7ae11173c2c257a868ba0
Jordy Araujo <jordy.araujo@hotmail.com>
2021-10-27 20:26:48 -0400

    Add queue initial state + prevent clicking on active patent

--- frontend/src/components/PatentPage/PatentQueue.jsx
+++ frontend/src/components/PatentPage/PatentQueue.jsx
@@ -4,6 +4,27 @@ import { Card, ListGroup } from "react-bootstrap";
 
 const PatentQueue = (props) => {
     const history = useHistory()
+    const queueIndex = history.location.state ? history.location.state['queueIndex'] : undefined;
+
+    // set the initial state of the queue:
+    if(props.patents !== undefined)
+    {
+        if(queueIndex === undefined)
+        {
+            if(props.patents[1].length > 0)
+            {
+                history.push({ // at this point we are loading the first item on the queue:
+                    pathname: '/Patents',
+                    state: { 
+                        queueIndex: 0,
+                        queueLength: props.patents[1].length
+                    }
+                })
+            }
+        }
+    }
+
+
     return (
         <Card style={{marginTop: "2%", width: "52%"}}>
             <ListGroup>
@@ -15,12 +36,13 @@ const PatentQueue = (props) => {
                                 key={index} 
                                 action variant="dark"
                                 active={item === props.patents[0].documentId}
+                                disabled={item === props.patents[0].documentId}
                                 onClick={() => {
                                     history.push({
                                         pathname: '/Patents',
                                         state: { 
-                                            patentId: item,
-                                            queueIndex: index 
+                                            queueIndex: index,
+                                            queueLength: props.patents[1].length
                                         }
                                     })
                                     history.go(0);
