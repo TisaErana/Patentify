a366069854a1e14982d97578e07ed7bee44c931f
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2022-03-10 00:56:59 -0500

    Fixed backend crash bug
    Backend would crash if token was incorrect. now user can clear input boxes and retype their token and new password and it will still update

--- backend/routes/passwordReset.js
+++ backend/routes/passwordReset.js
@@ -10,12 +10,13 @@ const sendEmail = require("../utils/sendEmail");
 const { UserSchema } = require("../models/User_model");
 const bcrypt = require('bcryptjs');
 const salt = bcrypt.genSaltSync(10);
-
 router.post("/requestPasswordLink", async (req, res) => {
     try {
         const user = await User.findOne({ email: req.body.email });
-        if (user == null)
+        if (user == null){
+            console.log("Email doesnt exist");
             return res.status(400).json({ error: 'User with given email does not exist.'});
+        }
             let token = await Token.findOne({ userId: user._id });
         if (token) await token.deleteOne();
         let resetToken = crypto.randomBytes(32).toString("hex");
@@ -31,10 +32,11 @@ router.post("/requestPasswordLink", async (req, res) => {
         const link = `${currentUrl}/ResetPage`;
         await sendEmail(user.email, 'Patentify Password Reset', `Hello ${user.name} , Please click the link below enter code below to reset your password\n ${link}\n${resetToken}`);
         res.send("password reset link sent to your email account");
+        console.log("Password reset email sent")
         return link;
     } catch (error) {
-        res.send("An error occured");
         console.log(error);
+        res.send("An error occured");
     }
 });
 
@@ -42,24 +44,25 @@ router.post("/requestPasswordLink", async (req, res) => {
 router.post("/verify/update", async (req, res) =>{
     let inputToken= req.body.token;
     let token = await Token.findOne({ token:inputToken});
-    let TokenUserId = token.userId;
-    console.log("UserId VARIABLE = token.userId : " + TokenUserId);
-    let user = await User.findOne({_id:TokenUserId});
-    console.log("user id : " + user._id);
-    let newPassword = req.body.password
-    console.log("New password = " + newPassword);
     if(!token){
         console.log("Token is not valid")
-        throw new Error("Invalid token");
+        res.send
     }
-    console.log("new Password: " + newPassword);
+    if(token){
+    let TokenUserId = token.userId;
+    let user = await User.findOne({_id:TokenUserId});
+    let newPassword = req.body.password
     const hashedPassword = bcrypt.hashSync(newPassword, salt);
-    console.log("Hashed Password: " + hashedPassword);
     await User.updateOne(
         {_id : user._id  },
         { $set: {password: hashedPassword} }
         )
-    
+        let loginLink = 'http://localhost:3000/Login';
+        await sendEmail(user.email, "Password reset confirmation", `Hello ${user.name} your password has been successfully updated, click the link below to login\n${loginLink}`);
+        res.status(200).send('confirmation email sent');
+        console.log("Reset confirmation email sent")
+
+    }
 
 });
 
