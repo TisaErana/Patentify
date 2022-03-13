909c0345753a40c31fe5a31e862fc7c77f752999
Jordy Araujo <jordy.araujo@hotmail.com>
2022-03-12 23:24:01 -0500

    Remove duplicate User import from merge of 'main' branch

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("../auth/passport/index");
-const { User, validate } = require("../models/User_model");
 const userconfirmed = require("../models/user_confirmed_model")
 const User = require("../models/User_model");
 const bcrypt = require('bcryptjs');
