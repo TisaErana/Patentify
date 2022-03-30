748881582a7d9feb81fe96871c094c04917768a5
Jordy Araujo <jordy.araujo@hotmail.com>
2022-03-30 17:30:48 -0400

    Add PGPUBs to import claims script

--- backend/svm/Scripts/import_claims.py
+++ backend/svm/Scripts/import_claims.py
@@ -17,7 +17,7 @@ matched_count_total = 0
 modified_count_total = 0
 
 USPATs = [element['documentId'] for element in list(dbPatents.find({"patentCorpus": "USPAT"}, {"_id": False, "documentId": 1}))]
-#USPATs = ['RE28686']
+print('Total USPATs in database:', len(USPATs))
 
 cycle = 0
 for patent in USPATs:
@@ -53,14 +53,57 @@ for patent in USPATs:
     )
   ], ordered=False)
 
-  print('\ Done with USPAT claim import cycle', cycle, '/')
+  print('--> Done with USPAT claim import cycle', cycle, '<--')
 
   cycle += 1
   matched_count_total += result.matched_count
   modified_count_total += result.modified_count
 
+print('Started processing application_claims_all.tsv...')
+print()
+
+PGPUBs = [element['documentId'] for element in list(dbPatents.find({"patentCorpus": "PGPUB"}, {"_id": False, "documentId": 1}))]
+print('Total PGPUBs in database:', len(PGPUBs))
+
+cycle = 0
+for patent in PGPUBs:
+  claims = []
+
+  # load USPAT claims from tsv file:
+  applicationClaims = pd.read_csv(
+    'data/application_claim_all.tsv',
+    header = 0, # header at row 0
+    sep = '\t',  # tab separated
+    chunksize = 10000000, # process in chunks
+    usecols = ['document_number', 'text'],
+    dtype = { 'document_number': str, 'text': str } # let's make it's all strings
+  )
+
+  for chunk in applicationClaims:
+    chunk = chunk[chunk['document_number'].isin([patent])]
+    
+    for entry in chunk.itertuples():
+      claims.append(entry.text.strip())
+
+  claims.sort(key=lambda x : int(x[0:2].strip()))
+  print(claims)
+
+  result = dbPatents.bulk_write([
+    UpdateOne({ "documentId": patent}, { 
+      "$set": { 
+          "claims": claims
+        } 
+      }        
+    )
+  ], ordered=False)
+
+  print('--> Done with PGPUB claim import cycle', cycle, '<--')
+
+  cycle += 1
+  matched_count_total += result.matched_count
+  modified_count_total += result.modified_count
 
 print()
-print('Matched', matched_count_total, 'USPATs.')
-print('Updated', modified_count_total, 'USPATs.')
+print('Matched', matched_count_total, 'documents.')
+print('Updated', modified_count_total, 'documents.')
 print()
