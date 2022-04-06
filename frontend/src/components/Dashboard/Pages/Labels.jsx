6896ee66d28e4468bfe6393c53f2f278fb954f93
Jordy Araujo <jordy.araujo@hotmail.com>
2022-04-06 19:33:58 -0400

    Fix tables, cohen's kappa calculation, export feature

--- frontend/src/components/Dashboard/Pages/Labels.jsx
+++ frontend/src/components/Dashboard/Pages/Labels.jsx
@@ -5,14 +5,17 @@ const Cohen = require('cohens-kappa');
 
 const Table = () => {
     
+    
+    const [data, setData] = useState();
     const [labels, setLabels] = useState();
-    const [agreedLabels, setAgreedLabels] = useState();
-    const [disagreedLabels, setDisagreedLabels] = useState();
+
+    const [agreedLabelsTableFormat, setAgreedLabelsTableFormat] = useState();
+    const [disagreedLabelsTableFormat, setDisagreedLabelsTableFormat] = useState();
 
     const labelColumns = [ 
       { title:'Label ID', field:'_id'},
       { title:'Document ID', field:'document', defaultGroupOrder:0},
-      { title:'User', field:'user', defaultGroupOrder:1},
+      { title:'User', field:'user.email', defaultGroupOrder:1},
       { title:'Date', field:'createdAt'},
       { title:'Machine Learning', field:'mal'},
       { title:'AI Hardware', field:'hdw'},
@@ -27,7 +30,7 @@ const Table = () => {
 
     const agreedColumns = [ 
       { title:'Document ID', field:'document', defaultGroupOrder:0},
-      { title:'Users', field:'userIds', defaultGroupOrder:1},
+      { title:'Users', field:'user.email'},
       { title:'Date', field:'createdAt'},
       { title:'Machine Learning', field:'mal'},
       { title:'AI Hardware', field:'hdw'},
@@ -41,6 +44,15 @@ const Table = () => {
 
     const disagreedColumns = [ 
       { title:'Document ID', field:'document', defaultGroupOrder:0},
+      { title:'User', field:'user.email'},
+      { title:'Machine Learning', field:'mal'},
+      { title:'AI Hardware', field:'hdw'},
+      { title:'Speech', field:'spc'},
+      { title:'Vision', field:'vis'},
+      { title:'Natural Language Processing', field:'nlp'},
+      { title:'Planning/Control', field:'pln'},
+      { title:'Evolutionary Computation', field:'evo'},
+      { title:'Knowledge Processing', field:'kpr'},
       { title:'Created At', field:'createdAt'},
       { title:'Updated At', field:'updatedAt'}
     ];
@@ -55,9 +67,63 @@ const Table = () => {
           const body = await response.json();
           // body is an object with the response 
           
+          // add user information to table data:
+          body.labels = body.labels.map(annotation => ({
+            ...annotation,
+            user: body.users.find(user => user._id === annotation.user)
+          }));
+
+          body.agreedLabels = body.agreedLabels.map(annotation => ({
+            ...annotation,
+            individual: annotation.individual.map(i => ({
+              ...i,
+              user: body.users.find(user => user._id === i.user)
+            }))
+          }));
+
+          body.disagreedLabels = body.disagreedLabels.map(annotation => ({
+            ...annotation,
+            disagreement: annotation.disagreement.map(d => ({
+              ...d,
+              user: body.users.find(user => user._id === d.user)
+            }))
+          }));
+          
+          // make each annotation an individual row so it is easier to understand:
+          body.agreedLabels.tableFormat = []
+          body.agreedLabels.forEach(annotation => {
+            annotation.individual.forEach(d => {
+              d.document = annotation.document;
+              d.createdAt = annotation.createdAt;
+              d.updatedAt = annotation.updatedAt;
+              body.agreedLabels.tableFormat.push(d);
+            });
+            annotation.consensus.document = annotation.document;
+            annotation.consensus.createdAt = annotation.createdAt;
+            annotation.consensus.updatedAt = annotation.updatedAt;
+            annotation.consensus.user = { email: '[Consensus]'}
+            body.agreedLabels.tableFormat.push(annotation.consensus);
+          });
+
+          body.disagreedLabels.tableFormat = []
+          body.disagreedLabels.forEach(annotation => {
+            annotation.disagreement.forEach(d => {
+              d.document = annotation.document;
+              d.createdAt = annotation.createdAt;
+              d.updatedAt = annotation.updatedAt;
+              body.disagreedLabels.tableFormat.push(d);
+            });
+            annotation.consensus.document = annotation.document;
+            annotation.consensus.createdAt = annotation.createdAt;
+            annotation.consensus.updatedAt = annotation.updatedAt;
+            annotation.consensus.user = {email: '[Consensus] ' + body.users.find(user => user._id === annotation.consensus.user).email };
+            body.disagreedLabels.tableFormat.push(annotation.consensus);
+          });
+
+          setData(body);
           setLabels(body.labels);
-          setAgreedLabels(body.agreedLabels);
-          setDisagreedLabels(body.disagreedLabels);
+          setAgreedLabelsTableFormat(body.agreedLabels.tableFormat);
+          setDisagreedLabelsTableFormat(body.disagreedLabels.tableFormat);
 
         } catch (error) {}
       }
@@ -76,8 +142,6 @@ const Table = () => {
             data={labels}
             isLoading={labels === undefined}
             options={{
-              exportButton: true,
-              exportAllData: true,
               grouping: true,
               selection: true,
             }}
@@ -100,19 +164,15 @@ const Table = () => {
 
                     const categories = ["Yes", "No"];
 
-                    let group = groupByKey(rowData, 'user')
-                 
-                    let array = Object.keys(group).map((key) => group[key])
-
-                    let user1 = array[0].map((x) => [x.mal, x.hdw ,x.evo ,x.spc ,x.vis, x.nlp ,x.pln, x.kpr, x.none]).flat()
-                    let user2 = array[1].map((x) => [x.mal, x.hdw ,x.evo ,x.spc ,x.vis, x.nlp ,x.pln, x.kpr, x.none]).flat()
+                    let values = rowData.map((x) => [x.mal, x.hdw ,x.evo ,x.spc ,x.vis, x.nlp ,x.pln, x.kpr])
+                    
+                    let user1 = values[0];
+                    let user2 = values[1];
                 
                     console.log(user1, user2)
 
-                    
                     let rev1numeric = Cohen.nominalConversion(categories,user1);
                     let rev2numeric = Cohen.nominalConversion(categories,user2);
-
 
                     let kappaUnweighted = Cohen.kappa(
                       rev1numeric,
@@ -125,8 +185,16 @@ const Table = () => {
                     
                   }
                   findKappa();
-                },
+                }
               },
+              {
+                icon: 'download',
+                tooltip: 'Export as JSON File',
+                isFreeAction: true,
+                onClick: (event, rowData) => {
+                  window.location = '/patents-api/export/labels';
+                }
+              }
             ]}
           />
 
@@ -134,13 +202,21 @@ const Table = () => {
           <MaterialTable
             title="Labels in Agreement"
             columns={agreedColumns}
-            data={agreedLabels}
-            isLoading={agreedLabels === undefined}
+            data={agreedLabelsTableFormat}
+            isLoading={agreedLabelsTableFormat === undefined}
+            actions={[
+              {
+                icon: 'download',
+                tooltip: 'Export as JSON File',
+                isFreeAction: true,
+                onClick: (event, rowData) => {
+                  window.location = '/patents-api/export/agreedLabels';
+                }
+              }
+            ]}
             options={{
-              exportButton: true,
-              exportAllData: true,
               grouping: true,
-              selection: true,
+              selection: false
             }}
           />
 
@@ -148,13 +224,21 @@ const Table = () => {
           <MaterialTable
             title="Labels in Disagreement"
             columns={disagreedColumns}
-            data={disagreedLabels}
-            isLoading={disagreedLabels === undefined}
+            data={disagreedLabelsTableFormat}
+            isLoading={disagreedLabelsTableFormat === undefined}
+            actions={[
+              {
+                icon: 'download',
+                tooltip: 'Export as JSON File',
+                isFreeAction: true,
+                onClick: (event, rowData) => {
+                  window.location = '/patents-api/export/disagreedLabels';
+                }
+              }
+            ]}
             options={{
-              exportButton: true,
-              exportAllData: true,
               grouping: true,
-              selection: true,
+              selection: false
             }}
           />
         </div>
