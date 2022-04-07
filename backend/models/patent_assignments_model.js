a46c5c822200d660a10d5e9ff27b27a6d4fc81ff
Jordy Araujo <jordy.araujo@hotmail.com>
2022-04-07 01:44:47 -0400

    Add patent assignment database model

---
+++ backend/models/patent_assignments_model.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require("mongoose");
+
+const patentAssignmentSchema = new Schema(
+  
+  {
+    user:{ type: Schema.Types.ObjectId, ref: "User", index: {unique: true, dropDups: true}},
+    assignments: [{type: String}]
+  },
+
+  { collection: "patent_assignments" }
+);
+
+module.exports = model("PatentAssignment", patentAssignmentSchema);
