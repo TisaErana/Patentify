3f34d6c786cccf6ba3aa4f981a745db73ad5a3de
Jordy Araujo <jordy.araujo@hotmail.com>
2022-04-06 20:13:38 -0400

    Add uncertain patents database model

---
+++ backend/models/uncertain_patents.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose");
+
+const uncertainPatentSchema = new Schema(
+  
+  {
+    documentId:{type:String, index: {unique: true, dropDups: true}},
+    title:{type:String},
+    claims:{ type: [String] },
+    abstract:{type:String},
+    patentCorpus:{type:String}
+  },
+
+  { collection: "uncertain_patents" }
+);
+
+module.exports = model("UncertainPatent", uncertainPatentSchema);
