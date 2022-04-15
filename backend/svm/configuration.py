0d6a91c9855b641cd6cd63b453229fe93223817c
Jordy Araujo <jordy.araujo@hotmail.com>
2022-04-14 23:22:46 -0400

    Update svm configuration file

--- backend/svm/configuration.py
+++ backend/svm/configuration.py
@@ -1,3 +1,4 @@
-# Configuration:
-MIN_TRAINING_SIZE = N_COMPONENTS = 4 # the minimum number of patents required to train the model, n_components = min(n_components, y_features) y_features = targets
-MIN_AUTO_SAVE_CYCLES = 10
+# Active Learning Configuration:
+MIN_TRAINING_SIZE = 4 # the minimum number of patents required to train the model.
+N_COMPONENTS = 2 # minimum number of features for the truncated svm. N_COMPONENTS < MIN_TRAINING_SIZE
+MIN_AUTO_SAVE_CYCLES = 10 # the minimum number of training cycles before the model is auto-saved to a file.
